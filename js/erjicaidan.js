1c8529a02e3e4393ea6a1b32b4c0a804ac3c6902
sss <1234567@qq.com>
2018-10-13 23:56:23 +0800

    二次菜单第一次提交

---
+++ js/erjicaidan.js
@@ -0,0 +1,70 @@
+// $(".cq").mouseenter(function(){
+//     $(this).next().css("display","block");
+// })
+// $(".region").mouseleave(function(){
+//     $(this).css("display","none");
+// })
+$('.p1').mouseenter(function(){   
+    //  alert()
+    $(this).next().css("display","block");
+    $(this).parent().find(".list1-b").css("display","none");
+    $(this).parent().find(".list1-c").css("display","none");
+    $(this).parent().find(".list1-d").css("display","none");
+    $(this).parent().find(".list1-e").css("display","none");
+    $(this).parent().find(".list1-f").css("display","none");  
+})
+$('.p2').mouseenter(function(){   
+    //  alert()
+    $(this).next().css("display","block");
+    $(this).parent().find(".list1-a").css("display","none");
+    $(this).parent().find(".list1-c").css("display","none");
+    $(this).parent().find(".list1-d").css("display","none");
+    $(this).parent().find(".list1-e").css("display","none");
+    $(this).parent().find(".list1-f").css("display","none"); 
+    
+})
+$('.p3').mouseenter(function(){   
+    //  alert()
+    $(this).next().css("display","block");
+    $(this).parent().find(".list1-a").css("display","none");
+    $(this).parent().find(".list1-b").css("display","none");
+    $(this).parent().find(".list1-d").css("display","none");
+    $(this).parent().find(".list1-e").css("display","none");
+    $(this).parent().find(".list1-f").css("display","none"); 
+    
+})
+$('.p4').mouseenter(function(){   
+    //  alert()
+    $(this).next().css("display","block");
+    $(this).parent().find(".list1-a").css("display","none");
+    $(this).parent().find(".list1-b").css("display","none");
+    $(this).parent().find(".list1-c").css("display","none");
+    $(this).parent().find(".list1-e").css("display","none");
+    $(this).parent().find(".list1-f").css("display","none"); 
+    
+})
+$('.p5').mouseenter(function(){   
+    //  alert()
+    $(this).next().css("display","block");
+    $(this).parent().find(".list1-a").css("display","none");
+    $(this).parent().find(".list1-b").css("display","none");
+    $(this).parent().find(".list1-d").css("display","none");
+    $(this).parent().find(".list1-c").css("display","none");
+    $(this).parent().find(".list1-f").css("display","none"); 
+    
+})
+$('.p6').mouseenter(function(){   
+    //  alert()
+    $(this).next().css("display","block");
+    $(this).parent().find(".list1-a").css("display","none");
+    $(this).parent().find(".list1-b").css("display","none");
+    $(this).parent().find(".list1-d").css("display","none");
+    $(this).parent().find(".list1-e").css("display","none");
+    $(this).parent().find(".list1-c").css("display","none"); 
+    
+})
+
+
+
+
+
